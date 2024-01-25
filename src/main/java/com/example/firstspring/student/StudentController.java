01f8c79db069ff833f52c2ed8499d86a0a4da26f
saraKord <sara.k1777@yahoo.com>
2024-01-25 20:32:40 +0330

    add adaptor

--- src/main/java/com/example/firstspring/student/StudentController.java
+++ src/main/java/com/example/firstspring/student/StudentController.java
@@ -1,19 +1,17 @@
 package com.example.firstspring.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class StudentController {
     @Autowired
     private StudentAdaptor adaptor;
+    @Autowired
     private final StudentRepository studentRepository;
-@Autowired
+
     public StudentController( StudentRepository studentRepository) {
     this.studentRepository = studentRepository;
 
@@ -22,5 +20,7 @@ public class StudentController {
     public void save(@RequestBody StudentDto studentDto) {
         studentRepository.save(adaptor.convert(studentDto));
     }
+
+
 
 }
--- src/main/java/com/example/firstspring/student/StudentService.java
+++ src/main/java/com/example/firstspring/student/StudentService.java
@@ -1,19 +1,22 @@
 package com.example.firstspring.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.LocalDate;
-import java.util.List;
+
 @Service
 public class StudentService {
+    @Autowired
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    public StudentEntity save( StudentEntity studentEntity){
+
+        return studentRepository.save(studentEntity);
     }
 
 }
